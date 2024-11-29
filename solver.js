b04b2f0bf961249b902297ba9508d460297b5ea8
tatyanaQ <tatyana.slabodchykava@gmail.com>
2024-11-29 17:38:39 +0200

    setting up

---
+++ solver.js
@@ -0,0 +1,10 @@
+const { readFile } = require("../utils");
+
+function solvePuzzle(inputFileName) {
+  const lines = readFile({ fileName: inputFileName, trimmed: true });
+  console.log(lines);
+}
+
+module.exports = {
+  solvePuzzle,
+};
